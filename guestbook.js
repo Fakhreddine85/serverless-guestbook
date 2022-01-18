78ed89beea627fd476bc0ca413a71afcc74834b2
Fakhreddine85 <64703898+Fakhreddine85@users.noreply.github.com>
2022-01-18 16:16:34 +0000

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://30d70d47.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
